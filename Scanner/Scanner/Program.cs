22b5ee8f3c6d0e083bc4caa74e94614f1d9d47ae
Ghiurcuta Andrei <ghiurcutaandrei@gmail.com>
2021-10-11 14:44:43 +0300

    initial commit

---
+++ Scanner/Scanner/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Scanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ST symbolTable = new ST(10);
+
+            Console.WriteLine(symbolTable.Add("x"));
+            Console.WriteLine(symbolTable.Add("y"));
+            Console.WriteLine(symbolTable.Add("xy"));
+            Console.WriteLine(symbolTable.Add("yx"));
+            Console.WriteLine(symbolTable.Add("yx"));
+            Console.WriteLine(symbolTable.Add("xyz"));
+
+            Console.WriteLine(symbolTable.Find("x"));
+            Console.WriteLine(symbolTable.Find("y"));
+            Console.WriteLine(symbolTable.Find("xy"));
+            Console.WriteLine(symbolTable.Find("yx"));
+            Console.WriteLine(symbolTable.Find("w"));
+        }
+    }
+}
---
+++ Scanner/Scanner/ST.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scanner
+{
+    public class ST
+    {
+        public int Capacity { get; set; }
+        public string[] table { get; set; }
+
+        public ST(int capacity)
+        {
+            this.Capacity = capacity;
+            this.table = new string[capacity];
+        }
+
+        public bool Add(string key)
+        {
+            int h = hashFunction(key);
+
+            while (table[h] != null)
+            {
+                if (table[h] == key)
+                {
+                    return false;
+                }
+
+                h++;
+                if (h >= Capacity)
+                {
+                    resize();
+                }
+            }
+
+            table[h] = key;
+            return true;
+        }
+
+        private void resize()
+        {
+
+        }
+
+        public void remove(string key)
+        {
+        }
+
+        public int Find(string key)
+        {
+            int h = hashFunction(key);
+
+            while (table[h] != null)
+            {
+                if (table[h] == key)
+                {
+                    return h;
+                }
+
+                h++;
+            }
+
+            return -1;
+        }
+
+        private int hashFunction(string key)
+        {
+            int sum = 0;
+            byte[] asciiBytes = Encoding.ASCII.GetBytes(key);
+            foreach (byte b in asciiBytes)
+            {
+                sum += b;
+            }
+            return sum % Capacity;
+        }
+    }
+}
